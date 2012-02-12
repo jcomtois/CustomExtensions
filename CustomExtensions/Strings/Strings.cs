1c67d39291d8cd703dd43c2a89d90b69fbab8743
jcomtois <jcomtois@cfpwood.com>
2012-02-12 18:26:41 +0000

    --HG-- rename : CustomExtensions/Class1.cs => CustomExtensions/Strings/Strings.cs extra : convert_revision : svn%3A6a6af809-1682-f043-b4f4-082ee724d500/CustomExtensions/trunk%40839

---
+++ CustomExtensions/Strings/Strings.cs
@@ -0,0 +1,76 @@
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CustomExtensions.Strings
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Encryptes a string using the supplied key. Encoding is done using RSA encryption.
+        /// </summary>
+        /// <param name="StringToEncrypt"> String that must be encrypted. </param>
+        /// <param name="Key"> Encryptionkey. </param>
+        /// <returns> A string representing a byte array separated by a minus sign. </returns>
+        /// <exception cref="ArgumentException">Occurs when stringToEncrypt or key is null or empty.</exception>
+        public static string Encrypt(this string StringToEncrypt, string Key)
+        {
+            if (string.IsNullOrEmpty(StringToEncrypt))
+            {
+                throw new ArgumentException("An empty string value cannot be encrypted.");
+            }
+
+            if (string.IsNullOrEmpty(Key))
+            {
+                throw new ArgumentException("Cannot encrypt using an empty key. Please supply an encryption key.");
+            }
+
+            var cspParameters = new CspParameters {KeyContainerName = Key};
+            using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider(cspParameters) {PersistKeyInCsp = true})
+            {
+                return BitConverter.ToString(rsaCryptoServiceProvider.Encrypt(Encoding.UTF8.GetBytes(StringToEncrypt), true));
+            }
+        }
+
+        /// <summary>
+        /// Decryptes a string using the supplied key. Decoding is done using RSA encryption.
+        /// </summary>
+        /// <param name="StringToDecrypt"> String that must be decrypted. </param>
+        /// <param name="Key"> Decryptionkey. </param>
+        /// <returns> The decrypted string or null if decryption failed. </returns>
+        /// <exception cref="ArgumentException">Occurs when stringToDecrypt or key is null or empty.</exception>
+        public static string Decrypt(this string StringToDecrypt, string Key)
+        {
+            if (string.IsNullOrEmpty(StringToDecrypt))
+            {
+                throw new ArgumentException("An empty string value cannot be encrypted.");
+            }
+
+            if (string.IsNullOrEmpty(Key))
+            {
+                throw new ArgumentException("Cannot decrypt using an empty key. Please supply a decryption key.");
+            }
+
+            var cspParameters = new CspParameters {KeyContainerName = Key};
+            try
+            {
+                using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider(cspParameters)
+                {
+                    PersistKeyInCsp = true
+                })
+                {
+                    var decryptArray = StringToDecrypt.Split(new[] { "-" }, StringSplitOptions.None);
+                    var decryptByteArray = Array.ConvertAll(decryptArray, (s => Convert.ToByte(byte.Parse(s, NumberStyles.HexNumber))));
+                    return Encoding.UTF8.GetString(rsaCryptoServiceProvider.Decrypt(decryptByteArray, true));
+                }
+            }
+            catch (CryptographicException)
+            {
+                return null;
+            }
+            
+        }
+    }
+}
---
+++ UnitTests/StringTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CustomExtensions.Strings;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class StringTests
+    {
+        [TestFixture]
+        public class EncryptAndDecrypt
+        {
+            private const string TestText = "The quick brown fox jumped over the lazy dog.  1234567890";
+            private const string TestKey = "TESTKEY";
+
+            [Test]
+            public void NullKeyEmptyKey()
+            {
+                Assert.Throws<ArgumentException>(() => TestText.Encrypt(string.Empty));
+                Assert.Throws<ArgumentException>(() => TestText.Encrypt(null));
+                Assert.Throws<ArgumentException>(() => TestText.Decrypt(string.Empty));
+                Assert.Throws<ArgumentException>(() => TestText.Decrypt(null));
+            }
+
+            [Test]
+            public void NullTextEmptyText()
+            {
+                const string nullString = null;
+                Assert.Throws<ArgumentException>(() => string.Empty.Encrypt(TestKey));
+                Assert.Throws<ArgumentException>(() => nullString.Encrypt(TestKey));
+                Assert.Throws<ArgumentException>(() => string.Empty.Decrypt(TestKey));
+                Assert.Throws<ArgumentException>(() => nullString.Decrypt(TestKey));
+            }
+
+            [Test]
+            public void StringEncryptAndDecrypt()
+            {
+                var encrypted = TestText.Encrypt(TestKey);
+                var decrypted = encrypted.Decrypt(TestKey);
+
+                Assert.AreEqual(decrypted, TestText);
+            }
+
+            [Test]
+            public void WrongKey()
+            {
+                const string wrongKey = TestKey+TestKey;
+                var encrypted = TestText.Encrypt(TestKey);
+                var decrypted = encrypted.Decrypt(wrongKey);
+
+                Assert.IsNull(decrypted);
+                Assert.AreNotEqual(decrypted, TestText);
+            }
+        }
+    }
+}
