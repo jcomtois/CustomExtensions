715ce2d1bc7f6a33bbe177c742dace1472267023
jcomtois <jcomtois@cfpwood.com>
2012-02-13 20:49:07 +0000

    --HG-- extra : convert_revision : svn%3A6a6af809-1682-f043-b4f4-082ee724d500/CustomExtensions/trunk%40850

---
+++ CustomExtensions/IEnumerable/IEnumerables.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomExtensions.IEnumerable
+{
+    /// <summary>
+    /// Extensions for IEnumerable members
+    /// </summary>
+    public static class IEnumerables
+    {
+        /// <summary>
+        /// Determines whether or not IEnumerable is null or empty in an efficient way
+        /// </summary>
+        /// <typeparam name="T">The Type of elements in IEnumerable</typeparam>
+        /// <param name="source">The sequence of values to examine</param>
+        /// <returns>True if <paramref name="source"/> is null or empty.</returns>
+        public static bool IsEmpty <T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            var genericCollection = source as ICollection<T>;
+            if (genericCollection != null)
+            {
+                return genericCollection.Count == 0;
+            }
+
+            var nonGenericCollection = source as ICollection;
+            if (nonGenericCollection != null)
+            {
+                return nonGenericCollection.Count == 0;
+            }
+
+            return !source.Any();
+        }
+
+        /// <summary>
+        /// Invokes a transform function on each element of a generic sequence and returns the maximum resulting value or default if empty
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value returned by <paramref name="selector"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to determine the maximum value of.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <param name="DefaultValue"> </param>
+        /// <returns>The nullable maximum value in the sequence or default value if provided.</returns>
+        public static TResult MaxOrDefault <TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, TResult DefaultValue = default(TResult))
+        {
+            return source.IsEmpty() ? DefaultValue : source.Max(selector);
+        }
+
+        /// <summary>
+        /// Invokes a transform function on each element of a generic sequence and returns the minimum resulting value or default if empty
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value returned by <paramref name="selector"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to determine the minimum value of.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <param name="DefaultValue"> </param>
+        /// <returns>The nullable minimum value in the sequence or default value if provided.</returns>
+        public static TResult MinOrDefault <TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, TResult DefaultValue = default(TResult))
+        {
+            return source.IsEmpty() ? DefaultValue : source.Min(selector);
+        }
+
+        /// <summary>
+        /// Invokes a transform function on each element of a generic sequence and returns the maximum resulting nullable value 
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value returned by <paramref name="selector"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to determine the maximum value of.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns>The nullable maximum value in the sequence.</returns>
+        public static TResult? NullableMax <TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector) where TResult : struct
+        {
+            return source.IsEmpty() ? (TResult?)null : source.Max(selector);
+        }
+
+        /// <summary>
+        /// Invokes a transform function on each element of a generic sequence and returns the minimum resulting nullable value 
+        /// </summary>
+        /// <typeparam name="TSource">The type of the value returned by <paramref name="selector"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to determine the minimum value of.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns>The nullable minimum value in the sequence.</returns>
+        public static TResult? NullableMin <TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector) where TResult : struct
+        {
+            return source.IsEmpty() ? (TResult?)null : source.Min(selector);
+        }
+    }
+}
