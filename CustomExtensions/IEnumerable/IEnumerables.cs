dbda5203e24af0b821925bab41e2186af3c7f31c
jcomtois <jcomtois@cfpwood.com>
2012-02-20 13:36:18 +0000

    --HG-- extra : convert_revision : svn%3A6a6af809-1682-f043-b4f4-082ee724d500/CustomExtensions/trunk%40858

--- CustomExtensions/IEnumerable/IEnumerables.cs
+++ CustomExtensions/IEnumerable/IEnumerables.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CustomExtensions.IEnumerable
+namespace CustomExtensions.IEnumerables
 {
     /// <summary>
     /// Extensions for IEnumerable members
