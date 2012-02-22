0165378b20c4e88c457f4b4657ccaa735b0b8024
jcomtois <jcomtois@cfpwood.com>
2012-02-22 20:53:19 +0000

    --HG-- extra : convert_revision : svn%3A6a6af809-1682-f043-b4f4-082ee724d500/CustomExtensions/trunk%40863

--- CustomExtensions/Properties/AssemblyInfo.cs
+++ CustomExtensions/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.12.0221.8")]
-[assembly: AssemblyFileVersion("1.12.0221.8")]
+[assembly: AssemblyVersion("1.12.0222.0")]
+[assembly: AssemblyFileVersion("1.12.0222.0")]
