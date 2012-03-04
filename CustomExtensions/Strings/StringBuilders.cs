d5bc27c0442dd77d487c5dffcd82f5745346aa64
jcomtois <jcomtois@cfpwood.com>
2012-03-04 07:08:54 +0000

    --HG-- extra : convert_revision : svn%3A6a6af809-1682-f043-b4f4-082ee724d500/CustomExtensions/trunk%40882

---
+++ CustomExtensions/Strings/StringBuilders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace CustomExtensions.Strings
+{
+    public static class StringBuilders
+    {
+        public static StringBuilder AddLine(this StringBuilder sb, string NewText = null)
+        {
+            if (sb == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (string.IsNullOrEmpty(NewText))
+            {
+                Console.WriteLine();
+                return sb.AppendLine();
+            }
+            Console.WriteLine(NewText);
+            return sb.AppendLine(NewText);
+        }
+    }
+}
