ce957971bcbd8b2e5883708c35de7597c4fbd651
jcomtois <jcomtois@cfpwood.com>
2012-09-14 10:41:32 -0400

    Revamped structure of DateTime section
    --HG--
    rename : CustomExtensions/Datetime/Datetimes.cs => CustomExtensions/ForDatetime/ForDateTime.cs

---
+++ CustomExtensions/ForDatetime/ForDateTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CustomExtensions.ForDatetime
+{
+    /// <summary>
+    /// Provides a set of static methods to use on <see cref="DateTime"/> type
+    /// </summary>
+    public static partial class ForDateTime
+    {
+    }
+}
---
+++ CustomExtensions/ForDatetime/FormatyyyMMdd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace CustomExtensions.ForDatetime
+{   
+    public static partial class ForDateTime
+    {
+        /// <summary>
+        /// Formats <paramref name="source"/> to a string in yyyyMMdd pattern.  Uses en-US for <see cref="CultureInfo"/>
+        /// </summary>
+        /// <param name="source"><see cref="DateTime"/> to parse.</param>
+        /// <returns><see cref="string"/> representing date as yyyyMMdd</returns>
+        public static string FormatyyyyMMdd(this DateTime source)
+        {            
+            var dateTimeFormatInfo = new CultureInfo("en-US", false).DateTimeFormat;
+            return source.ToString("yyyyMMdd", dateTimeFormatInfo);
+        }
+    }
+}
