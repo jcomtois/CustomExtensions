b6f5a319c97802e9880bc50b18563333d1627389
jcomtois <jcomtois@cfpwood.com>
2012-09-17 10:06:32 -0400

    Added Morelinq nuget

---
+++ CustomExtensions/ForIEnumerable/Append.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomExtensions.ForIEnumerable
+{
+    public static partial class ForIEnumerable
+    {
+        /// <summary>
+        /// Appends and element to a source
+        /// </summary>
+        /// <typeparam name="T">Type of source IEnumerable</typeparam>
+        /// <param name="source">IEnumerable of type T</param>
+        /// <param name="element">Element to append</param>
+        /// <returns>IEnumerable of type T with element appended to the end</returns>
+        public static IEnumerable<T> Append <T>(this IEnumerable<T> source, T element)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            foreach (var t in source)
+            {
+                yield return t;
+            }
+            yield return element;
+        }
+    }
+}
--- CustomExtensions/ForIEnumerable/ForIEnumerable.cs
+++ CustomExtensions/ForIEnumerable/ForIEnumerable.cs
@@ -7,27 +7,6 @@ namespace CustomExtensions.ForIEnumerable
 {
    public static partial class ForIEnumerable
     {
-        /// <summary>
-        /// Appends and element to a source
-        /// </summary>
-        /// <typeparam name="T">Type of source IEnumerable</typeparam>
-        /// <param name="source">IEnumerable of type T</param>
-        /// <param name="element">Element to append</param>
-        /// <returns>IEnumerable of type T with element appended to the end</returns>
-        public static IEnumerable<T> Append <T>(this IEnumerable<T> source, T element)
-        {
-            if (source == null)
-            {
-                throw new ArgumentNullException("source");
-            }
-
-            foreach (var t in source)
-            {
-                yield return t;
-            }
-            yield return element;
-        }
-
         public static decimal AverageOrDefault(this IEnumerable<decimal> source, decimal DefaultValue = default(decimal))
         {
             return source == null ? DefaultValue : source.DefaultIfEmpty(DefaultValue).Average();
