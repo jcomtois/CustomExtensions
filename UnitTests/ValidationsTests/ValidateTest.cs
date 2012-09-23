9b12577e5b6912aeb2f4b2836c7722e751f2f675
jcomtois <jcomtois@cfpwood.com>
2012-09-23 19:55:26 -0400

    Tested Validate

---
+++ UnitTests/ValidationsTests/ValidateTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CustomExtensions.Validation;
+using NUnit.Framework;
+
+namespace UnitTests.ValidationsTests
+{
+    public partial class ValidataionTests
+    {
+        [TestFixture]
+        public class ValidateTest
+        {
+            [Test]
+            public void ValidateBegin()
+            {
+                Assert.That(Validate.Begin(), Is.Null);
+            }
+
+            
+        }
+    }
+}
