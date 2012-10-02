456de116f536827ff5d37595151ceeed28a938d2
jcomtois <jcomtois@cfpwood.com>
2012-10-02 14:59:35 -0400

    Added more ToOrDefault Tests

--- UnitTests/ForIConvertablesTests/ToOrDefaultTest.cs
+++ UnitTests/ForIConvertablesTests/ToOrDefaultTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CustomExtensions.ForIConvertible;
+﻿using CustomExtensions.ForIConvertible;
 using NUnit.Framework;
 
 namespace UnitTests.ForIConvertablesTests
@@ -10,10 +9,17 @@ namespace UnitTests.ForIConvertablesTests
         public class ToOrDefaultTest
         {
             [Test]
-            public void ToOrDefaultInteger_OnString_ReturnsInteger()
+            public void ToOrDefaultInt_OnDouble_ReturnsDefaultInteger()
             {
-                string number = "1";
-                Assert.That(() => number.ToOrDefault<int>(), Is.EqualTo(1));
+                double number = double.MaxValue;
+                Assert.That(() => number.ToOrDefault<int>(), Is.EqualTo(default(int)));
+            }
+
+            [Test]
+            public void ToOrDefaultInteger_OnBadString_ReturnsDefaultInteger()
+            {
+                string number = "ABC";
+                Assert.That(() => number.ToOrDefault<int>(), Is.EqualTo(default(int)));
             }
 
             [Test]
@@ -31,24 +37,10 @@ namespace UnitTests.ForIConvertablesTests
             }
 
             [Test]
-            public void ToOrDefaultInteger_OnBadString_ReturnsDefaultInteger()
+            public void ToOrDefaultInteger_OnString_ReturnsInteger()
             {
-                string number = "ABC";
-                Assert.That(() => number.ToOrDefault<int>(), Is.EqualTo(default(int)));
-            }
-
-            [Test]
-            public void ToOrDefaultSingle_OnDouble_ReturnsPositiveInfinity()
-            {
-                double number = double.MaxValue;
-                Assert.That(() => number.ToOrDefault<float>(), Is.EqualTo(float.PositiveInfinity));
-            }
-
-            [Test]
-            public void ToOrDefaultInt_OnDouble_ReturnsDefaultInteger()
-            {
-                double number = double.MaxValue;
-                Assert.That(() => number.ToOrDefault<int>(), Is.EqualTo(default(int)));
+                string number = "1";
+                Assert.That(() => number.ToOrDefault<int>(), Is.EqualTo(1));
             }
 
             [Test]
@@ -57,7 +49,125 @@ namespace UnitTests.ForIConvertablesTests
                 double number = double.MaxValue;
                 Assert.That(() => number.ToOrDefault<int?>(), Is.Null);
             }
- 
+
+            [Test]
+            public void ToOrDefaultOutInt_OnDouble_ReturnsDefaultInteger()
+            {
+                double number = double.MaxValue;
+                int outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.EqualTo(default(int)));
+            }
+
+            [Test]
+            public void ToOrDefaultOutInt_OnDouble_ReturnsFalse()
+            {
+                double number = double.MaxValue;
+                int outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.False);
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnBadString_ReturnsDefaultInteger()
+            {
+                string number = "ABC";
+                int outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.EqualTo(default(int)));
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnBadString_ReturnsFalse()
+            {
+                string number = "ABC";
+                int outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.False);
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnEmptyString_ReturnsDefaultInteger()
+            {
+                string number = string.Empty;
+                int outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.EqualTo(default(int)));
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnEmptyString_ReturnsFalse()
+            {
+                string number = string.Empty;
+                int outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.False);
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnNullString_ReturnsDefaultInteger()
+            {
+                string number = null;
+                int outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.EqualTo(default(int)));
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnNullString_ReturnsFalse()
+            {
+                string number = null;
+                int outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.False);
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnString_ReturnsInteger()
+            {
+                string number = "1";
+                int outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.EqualTo(1));
+            }
+
+            [Test]
+            public void ToOrDefaultOutInteger_OnString_ReturnsTrue()
+            {
+                string number = "1";
+                int outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.True);
+            }
+
+            [Test]
+            public void ToOrDefaultOutNullableInt_OnDouble_ReturnsFalse()
+            {
+                double number = double.MaxValue;
+                int? outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.False);
+            }
+
+            [Test]
+            public void ToOrDefaultOutNullableInt_OnDouble_ReturnsNull()
+            {
+                double number = double.MaxValue;
+                int? outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.Null);
+            }
+
+            [Test]
+            public void ToOrDefaultOutSingle_OnDouble_ReturnsPositiveInfinity()
+            {
+                double number = double.MaxValue;
+                float outParameter;
+                number.ToOrDefault(out outParameter);
+                Assert.That(() => outParameter, Is.EqualTo(float.PositiveInfinity));
+            }
+
+            [Test]
+            public void ToOrDefaultOutSingle_OnDouble_ReturnsPositiveTrue()
+            {
+                double number = double.MaxValue;
+                float outParameter;
+                Assert.That(() => number.ToOrDefault(out outParameter), Is.True);
+            }
         }
     }
 }
