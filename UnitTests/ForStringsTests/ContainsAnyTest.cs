c12162703bef4dfb328ab6404f9ae16045d42eef
jcomtois <jcomtois@cfpwood.com>
2012-10-08 10:44:23 -0400

    Added tests for ContainsAny and changed spelling error
    --HG--
    rename : CustomExtensions/ForStrings/ConatainsAny.cs => CustomExtensions/ForStrings/ContainsAny.cs

---
+++ UnitTests/ForStringsTests/ContainsAnyTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using CustomExtensions.ForStrings;
+using CustomExtensions.Validation;
+using NUnit.Framework;
+
+namespace UnitTests.ForStringsTests
+{
+    public partial class StringTests
+    {
+        [TestFixture]
+        public class ContainsAnyTest
+        {
+            [Test]
+            public void ContainsAnyCharacter_OnEmptyString_ReturnsFalse()
+            {
+                string sourceString = string.Empty;
+                char toFind = 'Z';
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyCharacter_OnGoodString_FindsManyReturnsTrue()
+            {
+                string sourceString = "TEST";
+                char toFind = 'T';
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.True);
+            }
+
+            [Test]
+            public void ContainsAnyCharacter_OnGoodString_FindsNoneReturnsFalse()
+            {
+                string sourceString = "TEST";
+                char toFind = 'Z';
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyCharacter_OnGoodString_FindsOneReturnsTrue()
+            {
+                string sourceString = "TEST";
+                char toFind = 'E';
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.True);
+            }
+
+            [Test]
+            public void ContainsAnyCharacter_OnGoodString_IsCaseSensitive()
+            {
+                string sourceString = "TEST";
+                char toFind = 'e';
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyCharacter_OnNullString_ThrowsValidationException()
+            {
+                string sourceString = null;
+                char toFind = 'Z';
+                Assert.That(() => sourceString.ContainsAny(toFind), Throws.TypeOf<ValidationException>().With.InnerException.TypeOf<ArgumentNullException>());
+            }
+
+            [Test]
+            public void ContainsAnyCharacters_OnEmptyString_ReturnsFalse()
+            {
+                string sourceString = string.Empty;
+                char[] toFind = {'t', 'e'};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyCharacters_OnGoodString_FindsManyReturnsTrue()
+            {
+                string sourceString = "TEST";
+                char[] toFind = {'T', 'Z'};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.True);
+            }
+
+            [Test]
+            public void ContainsAnyCharacters_OnGoodString_FindsMultipleReturnsTrue()
+            {
+                string sourceString = "TEST";
+                char[] toFind = {'T', 'E'};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.True);
+            }
+
+            [Test]
+            public void ContainsAnyCharacters_OnGoodString_FindsNoneReturnsFalse()
+            {
+                string sourceString = "TEST";
+                char[] toFind = {'t', 'e'};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyCharacters_OnGoodString_FindsOneReturnsTrue()
+            {
+                string sourceString = "TEST";
+                char[] toFind = {'E', 'Z'};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.True);
+            }
+
+            [Test]
+            public void ContainsAnyCharacters_OnNullString_ThrowsValidationException()
+            {
+                string sourceString = null;
+                char[] toFind = {'t', 'e'};
+                Assert.That(() => sourceString.ContainsAny(toFind), Throws.TypeOf<ValidationException>().With.InnerException.TypeOf<ArgumentNullException>());
+            }
+
+            [Test]
+            public void ContainsAnyEmptyCharacters_OnEmptyString_ReturnsFalse()
+            {
+                string sourceString = string.Empty;
+                char[] toFind = {};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyEmptyCharacters_OnGoodString_ReturnsFalse()
+            {
+                string sourceString = "TEST";
+                char[] toFind = {};
+                Assert.That(() => sourceString.ContainsAny(toFind), Is.False);
+            }
+
+            [Test]
+            public void ContainsAnyNullCharacters_OnGoodString_ThrowsValidationException()
+            {
+                string sourceString = "TEST";
+                char[] toFind = null;
+                Assert.That(() => sourceString.ContainsAny(toFind), Throws.TypeOf<ValidationException>().With.InnerException.TypeOf<ArgumentNullException>());
+            }
+
+            [Test]
+            public void ContainsAnyNullCharacters_OnNullString_ThrowsValidationException()
+            {
+                string sourceString = null;
+                char[] toFind = null;
+                Assert.That(() => sourceString.ContainsAny(toFind), Throws.TypeOf<ValidationException>().With.InnerException.TypeOf<MultiException>());
+            }
+        }
+    }
+}
