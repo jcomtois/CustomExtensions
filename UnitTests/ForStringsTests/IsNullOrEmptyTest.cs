82176a6a854ee8598f5587c6b9ba067102c84fad
jcomtois <jcomtois@cfpwood.com>
2012-10-11 13:25:05 -0400

    IsNullOrEmpty Testing

---
+++ UnitTests/ForStringsTests/IsNullOrEmptyTest.cs
@@ -0,0 +1,34 @@
+﻿using CustomExtensions.ForStrings;
+using NUnit.Framework;
+
+namespace UnitTests.ForStringsTests
+{
+    public partial class StringTests
+    {
+        [TestFixture]
+        public class IsNullOrEmptyTest
+        {
+            private const string ValidString = "TestString";
+            private const string EmptyString = "";
+            private const string NullString = null;
+
+            [Test]
+            public void IsNullOrEmpty_OnEmptyString_ReturnsTrue()
+            {
+                Assert.That(() => EmptyString.IsNullOrEmpty(), Is.True);
+            }
+
+            [Test]
+            public void IsNullOrEmpty_OnNullString_ReturnsTrue()
+            {
+                Assert.That(() => NullString.IsNullOrEmpty(), Is.True);
+            }
+
+            [Test]
+            public void IsNullOrEmpty_OnValidString_ReturnsFalse()
+            {
+                Assert.That(() => ValidString.IsNullOrEmpty(), Is.False);
+            }
+        }
+    }
+}
