d7dd802c922a1e007930712826c094dcb16f8867
jcomtois <jcomtois@cfpwood.com>
2012-10-11 13:46:53 -0400

    Fixed a typo

--- UnitTests/ForStringsTests/IsValidUrlTest.cs
+++ UnitTests/ForStringsTests/IsValidUrlTest.cs
@@ -12,61 +12,61 @@ namespace UnitTests.ForStringsTests
             private const string EmptyString = "";
 
             [Test]
-            public void IsBadUrl_OnEmptyString_ReturnsFalse()
+            public void IsValidUrl_OnEmptyString_ReturnsFalse()
             {
                 Assert.That(() => EmptyString.IsValidUrl(), Is.False);
             }
 
             [Test]
-            public void IsBadUrl_OnMailTo_ReturnsTrue()
+            public void IsValidUrl_OnMailTo_ReturnsTrue()
             {
                 var testString = "mailto:fake@madeup.com";
                 Assert.That(() => testString.IsValidUrl(), Is.True);
             }
 
             [Test]
-            public void IsBadUrl_OnNormalTextString_ReturnsFalse()
+            public void IsValidUrl_OnNormalTextString_ReturnsFalse()
             {
                 var testString = "I'm a Bad URL.";
                 Assert.That(() => testString.IsValidUrl(), Is.False);
             }
 
             [Test]
-            public void IsBadUrl_OnNullString_ReturnsFalse()
+            public void IsValidUrl_OnNullString_ReturnsFalse()
             {
                 Assert.That(() => NullString.IsValidUrl(), Is.False);
             }
 
             [Test]
-            public void IsBadUrl_OnWebAddressMinusProtocol_ReturnsFalse()
+            public void IsValidUrl_OnWebAddressMinusProtocol_ReturnsFalse()
             {
                 var testString = "www.google.com";
                 Assert.That(() => testString.IsValidUrl(), Is.False);
             }
 
             [Test]
-            public void IsBadUrl_OnWebAddressWithMadeUpProtocol_ReturnsTrue()
+            public void IsValidUrl_OnWebAddressWithMadeUpProtocol_ReturnsTrue()
             {
                 var testString = "madeup://www.google.com";
                 Assert.That(() => testString.IsValidUrl(), Is.True);
             }
 
             [Test]
-            public void IsBadUrl_OnWebAddressWithftpProtocol_ReturnsTrue()
+            public void IsValidUrl_OnWebAddressWithftpProtocol_ReturnsTrue()
             {
                 var testString = "ftp://www.google.com";
                 Assert.That(() => testString.IsValidUrl(), Is.True);
             }
 
             [Test]
-            public void IsBadUrl_OnWebAddressWithhttpProtocol_ReturnsTrue()
+            public void IsValidUrl_OnWebAddressWithhttpProtocol_ReturnsTrue()
             {
                 var testString = "http://www.google.com";
                 Assert.That(() => testString.IsValidUrl(), Is.True);
             }
 
             [Test]
-            public void IsBadUrl_OnWebAddressWithhttpsProtocol_ReturnsTrue()
+            public void IsValidUrl_OnWebAddressWithhttpsProtocol_ReturnsTrue()
             {
                 var testString = "https://www.google.com";
                 Assert.That(() => testString.IsValidUrl(), Is.True);
