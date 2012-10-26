5e7b60f3bc0d669641efc8b53b50b8f8c9c6c684
jcomtois <jcomtois@cfpwood.com>
2012-10-26 19:27:46 -0400

    Implemented TryToGuid and TryToGuid Tests

--- CustomExtensions/ForStrings/TryToGuid.cs
+++ CustomExtensions/ForStrings/TryToGuid.cs
@@ -25,27 +25,69 @@ namespace CustomExtensions.ForStrings
     public static partial class ExtendString
     {
         /// <summary>
-        /// Attempts to convert a string to a guid.
+        /// Attempts to convert a string to a <see cref="Guid"/>.
         /// </summary>
         /// <param name="source">The string to try to convert</param>
-        /// <param name="resultGuid">Upon return will contain the Guid</param>
+        /// <param name="result">Upon return will contain the <see cref="Guid"/></param>
         /// <returns>Returns true if successful, otherwise false</returns>
-        public static bool TryToGuid(this string source, out Guid resultGuid)
+        /// <remarks>
+        /// The alphabetic hexadecimal digits in the <paramref name="source"/> parameter can be upper or lower case. 
+        /// For example, the following strings represent the same GUID:
+        /// 
+        /// "ca761232ed4211cebacd00aa0057b223" 
+        /// "CA761232-ED42-11CE-BACD-00AA0057B223" 
+        /// "{CA761232-ED42-11CE-BACD-00AA0057B223}" -- Prefer this version for speed
+        /// "(CA761232-ED42-11CE-BACD-00AA0057B223)" 
+        /// "{0xCA761232, 0xED42, 0x11CE, {0xBA, 0xCD, 0x00, 0xAA, 0x00, 0x57, 0xB2, 0x23}}" 
+        /// 
+        /// </remarks>
+        public static bool TryToGuid(this string source, out Guid result)
         {
             //ClsidFromString returns the empty guid for null strings   
             if (source.IsNullOrEmpty())
             {
-                resultGuid = Guid.Empty;
+                result = Guid.Empty;
                 return false;
             }
 
-            var hresult = NativeMethods.CLSIDFromString(source, out resultGuid);
+            var addBracesToSourceIfNeeded = source;
+
+            // Check for {}
+            if (addBracesToSourceIfNeeded[0] != '{')
+            {
+                addBracesToSourceIfNeeded = "{" + addBracesToSourceIfNeeded;
+            }
+
+            if (addBracesToSourceIfNeeded[addBracesToSourceIfNeeded.Length - 1] != '}')
+            {
+                addBracesToSourceIfNeeded += "}";
+            }
+
+            // Faster than managed version
+            var hresult = NativeMethods.CLSIDFromString(addBracesToSourceIfNeeded, out result);
             if (hresult >= 0)
             {
                 return true;
             }
-            resultGuid = Guid.Empty;
-            return false;
+
+            // Try Managed version
+
+            try
+            {
+                result = new Guid(source);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (ex is OverflowException ||
+                    ex is FormatException ||
+                    ex.GetType() == typeof (Exception))
+                {
+                    result = Guid.Empty;
+                    return false;
+                }
+                throw;
+            }
         }
     }
 }
