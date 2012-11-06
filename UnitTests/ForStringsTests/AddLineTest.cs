10630b6a33f91e7e703d15a4d8e9c7d296ad28f0
jcomtois <jcomtois@cfpwood.com>
2012-11-06 15:16:58 -0500

    AddLineTest Refactored

--- UnitTests/ForStringsTests/AddLineTest.cs
+++ UnitTests/ForStringsTests/AddLineTest.cs
@@ -18,12 +18,15 @@
 #endregion
 
 using System;
+using System.Linq;
 using System.Text;
 using CustomExtensions.ForStrings;
 using CustomExtensions.Interfaces;
 using CustomExtensions.Validation;
 using Moq;
 using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
 
 namespace UnitTests.ForStringsTests
 {
@@ -32,126 +35,160 @@ namespace UnitTests.ForStringsTests
         [TestFixture]
         public class AddLineTest
         {
-            #region Setup/Teardown
-
-            [SetUp]
-            public void SetUp()
-            {
-                _stringBuilder = new StringBuilder();
-            }
-
-            #endregion
-
-            private StringBuilder _stringBuilder;
+            private static readonly string LatinString = new string(Enumerable.Range(0, 256).Select(i => Convert.ToChar(Convert.ToByte(i))).ToArray());
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithEmptyStringNoLineWriter_ReturnsStringBuilder()
+            public void AddLine_OnGoodStringBuilderWithGoodString_WithLineWriter_ReturnsStringBuilder()
             {
-                string testText = EmptyTestString;
-                var output = _stringBuilder.AddLine(testText);
-                Assert.That(() => output, Is.EqualTo(_stringBuilder));
-            }
-
-            [Test]
-            public void AddLine_OnGoodStringBuilderWithEmptyString_CallsWriteLine()
-            {
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                fixture.Inject(LatinString);
+                var testString = fixture.CreateAnonymous<string>();
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
                 var mockLineWriter = new Mock<ILineWriter>();
-                string testText = EmptyTestString;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
+                ILineWriter lineWriter = mockLineWriter.Object;
+                var actual = stringBuilder.AddLine(testString, lineWriter);
+
+                Assert.That(() => actual, Is.EqualTo(stringBuilder));
+            }
+
+            [Test]
+            public void AddLine_OnGoodStringBuilder_WithEmptyString_NoLineWriter_ReturnsStringBuilder()
+            {
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
+                var emptyString = string.Empty;
+
+                var actual = stringBuilder.AddLine(emptyString);
+
+                Assert.That(() => actual, Is.EqualTo(stringBuilder));
+            }
+
+            [Test]
+            public void AddLine_OnGoodStringBuilder_WithEmptyString_WithLineWriter_CallsWriteLine()
+            {
+                var mockLineWriter = new Mock<ILineWriter>(MockBehavior.Strict);
+                var emptyString = string.Empty;
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
+                mockLineWriter.Setup(m => m.WriteLine());
+                ILineWriter lineWriter = mockLineWriter.Object;
+                stringBuilder.AddLine(emptyString, lineWriter);
+
                 mockLineWriter.Verify(m => m.WriteLine(), Times.Once());
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithEmptyString_ReturnsStringBuilder()
+            public void AddLine_OnGoodStringBuilder_WithEmptyString_WithLineWriter_ReturnsStringBuilder()
             {
                 var mockLineWriter = new Mock<ILineWriter>();
-                string testText = EmptyTestString;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
-                Assert.That(() => output, Is.EqualTo(_stringBuilder));
+                var emptyString = string.Empty;
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
+                ILineWriter lineWriter = mockLineWriter.Object;
+                var actual = stringBuilder.AddLine(emptyString, lineWriter);
+
+                Assert.That(() => actual, Is.EqualTo(stringBuilder));
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithEmptyString_StringBuilderContainsNewLine()
+            public void AddLine_OnGoodStringBuilder_WithEmptyString_WithLineWriter_StringBuilderContainsNewLine()
             {
                 var mockLineWriter = new Mock<ILineWriter>();
-                string testText = EmptyTestString;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
-                Assert.That(() => output.ToString(), Is.EqualTo(Environment.NewLine));
+                var emptyString = string.Empty;
+                var stringBuilder = new StringBuilder();
+                ILineWriter lineWriter = mockLineWriter.Object;
+                var actual = stringBuilder.AddLine(emptyString, lineWriter).ToString();
+
+                Assert.That(() => actual, Is.EqualTo(Environment.NewLine));
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithGoodStringNoLineWriter_ReturnsStringBuilder()
+            public void AddLine_OnGoodStringBuilder_WithGoodString_NoLineWriter_ReturnsStringBuilder()
             {
-                string testText = TestStringLatin;
-                var output = _stringBuilder.AddLine(testText);
-                Assert.That(() => output, Is.EqualTo(_stringBuilder));
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                fixture.Inject(LatinString);
+                var testString = fixture.CreateAnonymous<string>();
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
+                var actual = stringBuilder.AddLine(testString);
+
+                Assert.That(() => actual, Is.EqualTo(stringBuilder));
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithGoodString_CallsWriteLineWithString()
+            public void AddLine_OnGoodStringBuilder_WithGoodString_WithLineWriter_CallsWriteLineWithString()
             {
-                string testText = TestStringLatin;
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                fixture.Inject(LatinString);
+                var testString = fixture.CreateAnonymous<string>();
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
                 var mockLineWriter = new Mock<ILineWriter>();
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
+                ILineWriter lineWriter = mockLineWriter.Object;
+                stringBuilder.AddLine(testString, lineWriter);
 
-                mockLineWriter.Verify(m => m.WriteLine(testText), Times.Once());
+                mockLineWriter.Verify(m => m.WriteLine(testString), Times.Once());
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithGoodString_ReturnsStringBuilder()
+            public void AddLine_OnGoodStringBuilder_WithGoodString_WithLineWriter_StringBuilderContainsStringAndNewLine()
             {
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                fixture.Inject(LatinString);
+                var testString = fixture.CreateAnonymous<string>();
+                var stringBuilder = new StringBuilder();
                 var mockLineWriter = new Mock<ILineWriter>();
-                mockLineWriter.Setup(m => m.WriteLine(It.IsAny<string>()));
-                string testText = TestStringLatin;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
-                Assert.That(() => output, Is.EqualTo(_stringBuilder));
+                ILineWriter lineWriter = mockLineWriter.Object;
+                var actual = stringBuilder.AddLine(testString, lineWriter);
+
+                Assert.That(() => actual.ToString(), Is.EqualTo(testString + Environment.NewLine));
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithGoodString_StringBuilderContainsStringAndNewLine()
+            public void AddLine_OnGoodStringBuilder_WithNullString_WithLineWriter_CallsWriteLine()
             {
-                string testText = TestStringLatin;
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                string nullString = null;
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
                 var mockLineWriter = new Mock<ILineWriter>();
-                mockLineWriter.Setup(m => m.WriteLine(testText));
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
-                Assert.That(() => output.ToString(), Is.EqualTo(testText + Environment.NewLine));
-            }
+                ILineWriter lineWriter = mockLineWriter.Object;
+                stringBuilder.AddLine(nullString, lineWriter);
 
-            [Test]
-            public void AddLine_OnGoodStringBuilderWithNullString_CallsWriteLine()
-            {
-                var mockLineWriter = new Mock<ILineWriter>();
-                string testText = NullTestString;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
                 mockLineWriter.Verify(m => m.WriteLine(), Times.Once());
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithNullString_ReturnsStringBuilder()
+            public void AddLine_OnGoodStringBuilder_WithNullString_WithLineWriter_ReturnsStringBuilder()
             {
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                string nullString = null;
+                var stringBuilder = fixture.CreateAnonymous<StringBuilder>();
                 var mockLineWriter = new Mock<ILineWriter>();
-                string testText = NullTestString;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
-                Assert.That(() => output, Is.EqualTo(_stringBuilder));
+                ILineWriter lineWriter = mockLineWriter.Object;
+                var actual = stringBuilder.AddLine(nullString, lineWriter);
+
+                Assert.That(() => actual, Is.EqualTo(stringBuilder));
             }
 
             [Test]
-            public void AddLine_OnGoodStringBuilderWithNullString_StringBuilderContainsNewLine()
+            public void AddLine_OnGoodStringBuilder_WithNullString_WithLineWriter_StringBuilderContainsNewLine()
             {
+                string nullString = null;
+                var stringBuilder = new StringBuilder();
                 var mockLineWriter = new Mock<ILineWriter>();
-                string testText = NullTestString;
-                var output = _stringBuilder.AddLine(testText, mockLineWriter.Object);
-                Assert.That(() => output.ToString(), Is.EqualTo(Environment.NewLine));
+                ILineWriter lineWriter = mockLineWriter.Object;
+                var actual = stringBuilder.AddLine(nullString, lineWriter).ToString();
+
+                Assert.That(() => actual, Is.EqualTo(Environment.NewLine));
             }
 
             [Test]
-            public void AddLine_OnNullStringBuilderWithGoodString_ThrowsValidationException()
+            public void AddLine_OnNullStringBuilder_WithGoodString_WithLineWriter_ThrowsValidationException()
             {
-                var mockLineWriter = new Mock<ILineWriter>();
-                string testText = TestStringLatin;
+                var fixture = new Fixture().Customize(new CompositeCustomization(new MultipleCustomization(), new AutoMoqCustomization()));
+                fixture.Inject(LatinString);
+                var testString = fixture.CreateAnonymous<string>();
+                ILineWriter lineWriter = fixture.CreateAnonymous<ILineWriter>();
                 StringBuilder testBuilder = null;
-                Assert.That(() => testBuilder.AddLine(testText, mockLineWriter.Object),
-                            Throws.TypeOf<ValidationException>().With.InnerException.TypeOf<ArgumentNullException>());
+                Assert.That(() => testBuilder.AddLine(testString, lineWriter), Throws.TypeOf<ValidationException>().With.InnerException.TypeOf<ArgumentNullException>());
             }
         }
     }
